f6b93965904eca473d1c2e601912b5e7dc6dff06
Audrey Tan <audreyhtan@gmail.com>
2013-05-27 16:54:50 -0500

    Transforms homework wk 7

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ $(document).ready(function(event){
 
 // Camera Toggle
   $('.btn').click(function(){
-    $('img').toggle();
+    $('.wayne').toggle();
     $(this).text(function(i, text){
       return text === "Camera 1" ? "Camera 2" : "Camera 1";
-    })
+    });
     $(this).toggleClass('warn');
   });
 
