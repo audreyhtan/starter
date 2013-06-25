5d4f56bffea21347bae2dde35dd5080f540de049
Audrey Tan <audreyhtan@gmail.com>
2013-06-25 01:07:19 -0500

    transitions animations lesson

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(event){
 
 // Camera Toggle
-  $('.btn').click(function(){
+  $('.btn.btn-info.camera').click(function(){
     $('.wayne').toggle();
     $(this).text(function(i, text){
       return text === "Camera 1" ? "Camera 2" : "Camera 1";
     });
     $(this).toggleClass('warn');
+  });
+
+// Stopwatch
+  $(".btn.btn-success.clock").click(function(){
+    $(".second-hand").toggleClass('paused');
+    $(this).text(function(i, text){
+      return text === "Start" ? "Stop" : "Start";
+    });
+    $(this).toggleClass('btn-success').toggleClass('btn-danger');
   });
 
 
